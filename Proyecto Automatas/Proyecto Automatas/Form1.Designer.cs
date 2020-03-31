678006ca84a123774d940d9b6e180eab1483d390
J-A-C-E <J-A-C-E@users.noreply.github.com>
2020-03-30 19:13:39 -0600

    PRUEBAS :´v

--- Proyecto Automatas/Proyecto Automatas/Form1.Designer.cs
+++ Proyecto Automatas/Proyecto Automatas/Form1.Designer.cs
@@ -29,11 +29,22 @@
         private void InitializeComponent()
         {
             this.btnConectar = new System.Windows.Forms.Button();
+            this.txtFrase = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSubcadena = new System.Windows.Forms.TextBox();
+            this.txtToken = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtArchivo = new System.Windows.Forms.TextBox();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnConectar
             // 
-            this.btnConectar.Location = new System.Drawing.Point(405, 62);
+            this.btnConectar.Location = new System.Drawing.Point(531, 120);
             this.btnConectar.Name = "btnConectar";
             this.btnConectar.Size = new System.Drawing.Size(167, 23);
             this.btnConectar.TabIndex = 0;
@@ -41,22 +52,139 @@
             this.btnConectar.UseVisualStyleBackColor = true;
             this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
             // 
+            // txtFrase
+            // 
+            this.txtFrase.Location = new System.Drawing.Point(12, 12);
+            this.txtFrase.Multiline = true;
+            this.txtFrase.Name = "txtFrase";
+            this.txtFrase.Size = new System.Drawing.Size(159, 103);
+            this.txtFrase.TabIndex = 1;
+            this.txtFrase.Text = "SI (x>10) ENTONCES\r\nESCRIBE \"MAYOR\"\r\nSINO\r\nESCRIBE \"MENOR\"\r\nFINSI";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(354, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Lenght";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Subcadena a Evaluar";
+            // 
+            // txtSubcadena
+            // 
+            this.txtSubcadena.Location = new System.Drawing.Point(12, 165);
+            this.txtSubcadena.Name = "txtSubcadena";
+            this.txtSubcadena.Size = new System.Drawing.Size(159, 20);
+            this.txtSubcadena.TabIndex = 4;
+            // 
+            // txtToken
+            // 
+            this.txtToken.Location = new System.Drawing.Point(12, 223);
+            this.txtToken.Name = "txtToken";
+            this.txtToken.Size = new System.Drawing.Size(100, 20);
+            this.txtToken.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Token";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 269);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Archivo de Tokens";
+            // 
+            // txtArchivo
+            // 
+            this.txtArchivo.Location = new System.Drawing.Point(12, 285);
+            this.txtArchivo.Multiline = true;
+            this.txtArchivo.Name = "txtArchivo";
+            this.txtArchivo.Size = new System.Drawing.Size(100, 117);
+            this.txtArchivo.TabIndex = 8;
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(180, 57);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnIniciar.TabIndex = 9;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(177, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Numero de Renglon";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(180, 31);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 11;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.txtArchivo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtToken);
+            this.Controls.Add(this.txtSubcadena);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtFrase);
             this.Controls.Add(this.btnConectar);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.TextBox txtFrase;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSubcadena;
+        private System.Windows.Forms.TextBox txtToken;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtArchivo;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- Proyecto Automatas/Proyecto Automatas/Form1.cs
+++ Proyecto Automatas/Proyecto Automatas/Form1.cs
@@ -19,8 +19,20 @@ namespace Proyecto_Automatas
             InitializeComponent();
         }
 
+        public void Lenght()
+        {
+            string Cadena = txtFrase.Text;
+            MessageBox.Show(Cadena.Length.ToString());
+        }
+
         private void btnConectar_Click(object sender, EventArgs e)
         {
+            string Cadena;
+
+            txtFrase.Text = txtFrase.Text.Trim();
+            Cadena = txtFrase.Text;
+            //MessageBox.Show(Cadena.Length.ToString());
+
             string servidor = "107.180.51.242";
             string puerto = "3306";
             string usuario = "Javier";
@@ -61,5 +73,58 @@ namespace Proyecto_Automatas
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //NO SIRVE EL TRIM :(
+            string Cadena = txtFrase.Text.Trim();
+
+            for(int i=0; i<Cadena.Length; i++)
+            {
+                //Muestra el recorrido del textBox
+                MessageBox.Show(Cadena[i].ToString());
+                if (Cadena[i] == ' ')
+                {
+                    MessageBox.Show("Aqui hay un espacio");
+                }
+            }
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            string Cadena = txtFrase.Text.Trim();
+
+            //CICLO DE RECORRIDO
+            for (int i = 0; i < Cadena.Length; i++)
+            {
+                if (Cadena[i] == ' ')
+                {
+                    MessageBox.Show("ESPACIO");
+                    txtArchivo.Text = txtArchivo.Text + Cadena[i].ToString();
+
+                    txtSubcadena.Text = "";
+                }
+                else
+                {
+                    MessageBox.Show(Cadena[i].ToString());
+
+                    //MUESTRA el el recorrido en textBox
+                    txtSubcadena.Text = txtSubcadena.Text + Cadena[i].ToString();
+                }
+
+                /*
+                //Muestra el recorrido del textBox
+                MessageBox.Show(Cadena[i].ToString());
+
+                txtSubcadena.Text = txtSubcadena.Text + Cadena[i].ToString();
+                if (Cadena[i] == ' ')
+                {
+                    MessageBox.Show("Aqui hay un espacio");
+                }
+                */
+            }
+        }
+
+       
     }
 }
