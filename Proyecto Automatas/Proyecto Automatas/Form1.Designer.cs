0ed252bf07aa51743514de8e8556d802b589c8be
Eduardo M <41967019+EDDXD@users.noreply.github.com>
2020-04-05 13:29:27 -0500

    javi gei

--- Proyecto Automatas/Proyecto Automatas/Form1.Designer.cs
+++ Proyecto Automatas/Proyecto Automatas/Form1.Designer.cs
@@ -45,6 +45,7 @@
             this.btnArchivo = new System.Windows.Forms.Button();
             this.cmbArchivo = new System.Windows.Forms.ComboBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -159,6 +160,7 @@
             // 
             // txtRenglon
             // 
+            this.txtRenglon.Enabled = false;
             this.txtRenglon.Location = new System.Drawing.Point(537, 93);
             this.txtRenglon.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtRenglon.Name = "txtRenglon";
@@ -216,11 +218,23 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(390, 438);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 35);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Limpiar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.txtRenglon);
@@ -266,6 +280,7 @@
         private System.Windows.Forms.Button btnArchivo;
         private System.Windows.Forms.ComboBox cmbArchivo;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
